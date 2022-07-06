e83450b069e7c566ae9ecf47144b941916575dfa
rei-kurti <82828166+rei-kurti@users.noreply.github.com>
2022-07-06 21:47:23 +0200

    Add files via upload

---
+++ Word memory.py
@@ -0,0 +1,28 @@
+'''
+This program will test the users memory when remembering words. It will select a random word from a list and will ask the user if the word has been previously displayed or not. If the user
+answers incorrectly a life will be deducted. The game ends when the user has no more lifes remaining. The program has a count variable that will be used to forcefully display a previous word 
+every 2-4 words.
+'''
+import random
+
+def play():
+    all_words = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard"]#,"aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","abusive","acceptable","accessible","accidental","accurate","acid","acidic","acoustic","acrid","actually","ad hoc","adamant","adaptable","addicted","adhesive","adjoining","adorable","adventurous","afraid","aggressive","agonizing","agreeable","ahead","ajar","alcoholic","alert","alike","alive","alleged","alluring","aloof","amazing","ambiguous","ambitious","amuck","amused","amusing","ancient","angry","animated","annoyed","annoying","anxious","apathetic","aquatic","aromatic","arrogant","ashamed","aspiring","assorted","astonishing","attractive","auspicious","automatic","available","average","awake","aware","awesome","awful","axiomatic","bad","barbarous","bashful","bawdy","beautiful","befitting","belligerent","beneficial","bent","berserk","best","better","bewildered","big","billowy","bite-sized","bitter","bizarre","black","black-and-white","bloody","blue","blue-eyed","blushing","boiling","boorish","bored","boring","bouncy","boundless","brainy","brash","brave","brawny","breakable","breezy","brief","bright","bright","broad","broken","brown","bumpy","burly","bustling","busy","cagey","calculating","callous","calm","capable","capricious","careful","careless","caring","cautious","ceaseless","certain","changeable","charming","cheap","cheerful","chemical","chief","childlike","chilly","chivalrous","chubby","chunky","clammy","classy","clean","clear","clever","cloistered","cloudy","closed","clumsy","cluttered","coherent","cold","colorful","colossal","combative","comfortable","common","complete","complex","concerned","condemned","confused","conscious","cooing","cool","cooperative","coordinated","courageous","cowardly","crabby","craven","crazy","creepy","crooked","crowded","cruel","cuddly","cultured","cumbersome","curious","curly","curved","curvy","cut","cute","cute","cynical","daffy","daily","damaged","damaging","damp","dangerous","dapper","dark","dashing","dazzling","dead","deadpan","deafening","dear","debonair","decisive","decorous","deep","deeply","defeated","defective","defiant","delicate","delicious","delightful","demonic","delirious","dependent","depressed","deranged","descriptive","deserted","detailed","determined","devilish","didactic","different","difficult","diligent","direful","dirty","disagreeable","disastrous","discreet","disgusted","disgusting","disillusioned","dispensable","distinct","disturbed","divergent","dizzy","domineering","doubtful","drab","draconian","dramatic","dreary","drunk","dry","dull","dusty","dusty","dynamic","dysfunctional","eager","early"]
+    prev_words = set()
+    lives = 3
+    count = 2 # Initialized at 2 so we have at least 2 new words at the beginning
+    while lives > 0:
+        if count == 0:
+            word = random.choice(prev_words)
+            count = random.randint(2,4)
+        else:
+            word = random.choice(all_words)
+            count -= 1
+        print(word, f"You have {lives} lives.")
+        ans = input("Was this word displayed previously /(yes / no /) ")
+        if((ans == "yes" and word in prev_words) or (ans == "no" and (not word in prev_words))):
+            pass
+        else:
+            lives -= 1
+        prev_words.add(word)
+    print("You lose. Thanks for playing!")
+play() 
